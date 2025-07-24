51b9eb03e9621e3c7fbf43381691ad9ed014b9e2
Alban <alban.puech@epfl.ch>
2025-07-24 14:31:25 +0200

    fixed typo

--- gridfm_datakit/utils/stats.py
+++ gridfm_datakit/utils/stats.py
@@ -62,7 +62,6 @@ def plot_stats(base_path: str) -> None:
 
 
 class Stats:  # network stats
-class Stats:  # network stats
     """A class to track and analyze statistics related to power grid networks.
 
     This class maintains data lists of various network metrics including
