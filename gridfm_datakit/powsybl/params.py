5230def72c5a9363d16ab45690436a347581da47
tren <tengxiang.ren@artelys.com>
2026-07-10 11:07:01 -0400

    update get_default_lf_params comments
    Signed-off-by: tren <tengxiang.ren@artelys.com>

--- gridfm_datakit/powsybl/params.py
+++ gridfm_datakit/powsybl/params.py
@@ -16,9 +16,10 @@ def get_default_lf_params():
     # Otherwise, (e.g. a network initially configured for distributed slack),
     # the bus with the largest generator will be selected as single slack.
     
-    # By default, the most meshed bus would be selected as slack to ease branch-related convergence.
-    # The power flow result processing attaches the slack result to a generator's output, 
-    # hence the choice of the bus with the largest generator as slack.
+    # By default, powsybl would select the most meshed bus as slack to ease branch-related convergence.
+    # It could be a bus without any generator.
+    # The gridfm's power flow data processing expects the slack to be associated with a generator, 
+    # hence the choice of the bus with the largest generator as slack to avoid conflict.
     return pp.loadflow.Parameters(distributed_slack=False,
                                   read_slack_bus=True,
                                   write_slack_bus=True,
