b1ab86c8ff15789d38aae9ec1bfec94191613c63
tren <tengxiang.ren@artelys.com>
2026-07-17 13:57:35 -0400

    chore: format powsybl/params.py
    Signed-off-by: tren <tengxiang.ren@artelys.com>

--- gridfm_datakit/powsybl/params.py
+++ gridfm_datakit/powsybl/params.py
@@ -21,10 +21,11 @@ def get_default_lf_params():
     # The gridfm's power flow data processing expects the slack to be associated with a generator,
     # hence the choice of the bus with the largest generator as slack to avoid conflict.
     provider_params = {
-        'slackBusSelectionMode': 'LARGEST_GENERATOR', # default: MOST_MESHED
+        "slackBusSelectionMode": "LARGEST_GENERATOR",  # default: MOST_MESHED
     }
-    return pp.loadflow.Parameters(distributed_slack=False,
-                                  read_slack_bus=True,
-                                  write_slack_bus=True,
-                                  provider_parameters=provider_params,
-                                  )
+    return pp.loadflow.Parameters(
+        distributed_slack=False,
+        read_slack_bus=True,
+        write_slack_bus=True,
+        provider_parameters=provider_params,
+    )
