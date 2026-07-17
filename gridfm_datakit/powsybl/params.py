d6c0602143c8729e7ecd96ceb9bec404fa567df5
tren <tengxiang.ren@artelys.com>
2026-07-17 10:23:09 -0400

    Apply linting fixes
    Signed-off-by: tren <tengxiang.ren@artelys.com>

--- gridfm_datakit/powsybl/params.py
+++ gridfm_datakit/powsybl/params.py
@@ -15,14 +15,16 @@ def get_default_lf_params():
     # If a single slack is specified, it will be used.
     # Otherwise, (e.g. a network initially configured for distributed slack),
     # the bus with the largest generator will be selected as single slack.
-    
-    # By default, powsybl would select the most meshed bus as slack to ease branch-related convergence.
+
+    # By default, powsybl selects the most meshed bus as slack to ease branch-related convergence.
     # It could be a bus without any generator.
-    # The gridfm's power flow data processing expects the slack to be associated with a generator, 
+    # The gridfm's power flow data processing expects the slack to be associated with a generator,
     # hence the choice of the bus with the largest generator as slack to avoid conflict.
+    provider_params = {
+        'slackBusSelectionMode': 'LARGEST_GENERATOR', # default: MOST_MESHED
+    }
     return pp.loadflow.Parameters(distributed_slack=False,
                                   read_slack_bus=True,
                                   write_slack_bus=True,
-                                  provider_parameters={
-                                      'slackBusSelectionMode': 'LARGEST_GENERATOR' # default: MOST_MESHED
-                                  })
+                                  provider_parameters=provider_params,
+                                  )
